9644c709d137fdaf697017ca43e3fae9f7b6636b
Mohamed ShamsEldeen <mmm.emad64@gmail.com>
2023-08-11 18:51:40 +0300

    add signIn and signUp and sum sections

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+//app/page.tsx
+
+
+export default function Home() {
+  return (
+    <>
+     <h1 className='hed-text text-left'>Home</h1>
+    </>
+  )
+}
---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,5 @@
+function Bottmobar() {
+    return <h1>Bottombar</h1>
+}
+
+export default Bottmobar;
